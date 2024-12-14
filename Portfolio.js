aed39f3e44ae12b9787033d4fa4c7b864e28164c
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2024-12-15 04:50:16 +0530

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,19 +1,12 @@
-
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-} 
-
-
-
-
-
+}
 
 window.onscroll = () => {
-
     let sections = document.querySelectorAll('section');
     let navLinks = document.querySelectorAll('header nav a');
         
@@ -23,14 +16,11 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
         
-        if(top >= offset && top < offset + height)
-        {
+        if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
-
             });
         }
-    }
-        );
+    });
 }
